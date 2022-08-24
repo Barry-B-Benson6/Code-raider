e0a8de0c45205feac9565433b89ec654aeaa4c81
Barry-B-Benson6 <josh2chapman@gmail.com>
2022-08-24 23:32:08 +0930

    Moved token to .env file

--- Bot/main.js
+++ Bot/main.js
@@ -1,5 +1,6 @@
 const { channel } = require('diagnostics_channel');
 const Discord = require('discord.js');
+require('dotenv').config()
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGE_REACTIONS"] });
 
 const allCode = require('./codes.js').varToExport;
@@ -62,80 +63,15 @@ for (const file of commandFiles) {
 
 
 client.once('ready', () => {
-    // let guilds = client.guilds.cache
-    // guilds.forEach(g => {
-    //     console.log(g.name)
-    // });
     console.log('Refactor');
-
-//    const Guilds = client.guilds.cache.map(guild => guild.name);
-    //console.log(Guilds);
-
-//    client.guilds.cache.forEach(guild => {
-//        console.log(guild.name + " " + guild.id);
-//        let channels = guild.channels.cache.map(channel => channel.name)
-//        console.log(channels);
-//    });
-
-    let guild = client.guilds.cache.get('924076146323255296');
-
-    let members = client.guilds.cache.map(member => guild.members);
-    console.log(members);
-
-//    let general = client.channels.cache.get('924076146323255300');
-
-//    general.send("who the fuck names a server like that?");
-
-//    console.log("sent");
-//    let channels = guild.channels.cache.map(channel => channel.id);
-//    let channelNames = guild.channels.cache.map(channel => channel.name);
-
-//    console.log(channels);
-//    console.log(channelNames);
-
-    
-
     setInterval(() => {
             client.user.setActivity(`-setup to create a raid`);
     }, 2000);
 });
 
-const ServersFolders = fs.readdirSync('./Servers/').filter(file => file.endsWith('.js'));
-
 client.on('messageCreate', message => {
 
-<<<<<<< Updated upstream
-    console.log("message recieved")
-
-    if (message.author.bot) return;
-
-    console.log(message)
-
-    var channelFile = fs.mkdirSync(`./Servers/${message.guild.name}`, { recursive: true })
-    console.log(message.guild.name)
-
-    content =  `${message.author.username}: ${message.content} \n\r`
-
-    if (message.attachments.length !== 0){
-        for (const attachment of message.attachments){
-            content += `    ${attachment[1].attachment} \n\r`
-        }
-    }
-    fs.appendFile(`Servers/${message.guild.name}/${message.channel.name}.txt`,content, err => {
-        if (err) {
-            console.error(err)
-        }
-        //file written successfully
-    })
-
-    if (!message.content.startsWith(prefix)) return;
-=======
-    if (message.guild.guildId === 924076146323255296){
-        console.log(message.content);
-    }
-
     if (!message.content.startsWith(prefix) || message.author.bot) return;
->>>>>>> Stashed changes
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
@@ -182,5 +118,4 @@ client.on("interactionCreate", (interaction) => {
     }
 
 });
-
-client.login('OTEyNTI3NDA3NjM2OTcxNTIx.GioioF.M1EJM20pKSszSShEXLO3VIhxB3h-oPclIYQ-iw');
+client.login(process.env.TOKEN);
